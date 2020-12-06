1d9c59a8d849c9d5b9381ed1ceadbd3447847239
Tim Mortimer <tim@kiteframe.co.uk>
2020-12-06 11:36:25 +0000

    Solve Day 6 - Custom Customs

---
+++ day-6-custom-customs/customcustoms/CustomCustoms.java
@@ -0,0 +1,32 @@
+package customcustoms;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class CustomCustoms {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = new FileInputStream("day-6-custom-customs/input.txt");
+        Reader reader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line;
+        int lineCount = 0;
+
+        int questionCountSum = 0;
+        HashMap<Character, Boolean> questionCounts = new HashMap<>();
+
+        while((line = bufferedReader.readLine()) != null) {
+            lineCount += 1;
+            if (line.isEmpty()) {
+                questionCountSum += questionCounts.keySet().size();
+                questionCounts = new HashMap<>();
+            }
+
+            for(Character character: line.toCharArray()) {
+                questionCounts.put(character, true);
+            }
+        }
+
+        questionCountSum += questionCounts.keySet().size();
+        System.out.println(questionCountSum);
+    }
+}
