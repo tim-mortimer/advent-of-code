7d7ed2d427d52bb4fb7a52cc47a26c698fd2fbbb
Tim Mortimer <tim@kiteframe.co.uk>
2020-12-13 15:22:13 +0000

    Solve Day 13 - Shuttle Search Part 2

--- day-13-shuttle-search/shuttlesearch/ShuttleSearch.java
+++ day-13-shuttle-search/shuttlesearch/ShuttleSearch.java
@@ -1,6 +1,7 @@
 package shuttlesearch;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,18 +16,19 @@ public class ShuttleSearch {
         int estimatedDepartTime = Integer.parseInt(bufferedReader.readLine());
 
         List<Integer> busIds = Arrays.stream(bufferedReader.readLine().split(","))
-                .filter(id -> !id.equals("x"))
+                .map(id -> id.equals("x") ? "0" : id)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-
-        System.out.println(estimatedDepartTime);
-        System.out.println(busIds);
 
         boolean matchFound = false;
         int currentTime = estimatedDepartTime;
 
         while (!matchFound) {
-            for (int busId: busIds) {
+            for (int busId : busIds) {
+                if (busId == 0) {
+                    continue;
+                }
+
                 if (currentTime % busId == 0) {
                     System.out.println((currentTime - estimatedDepartTime) * busId);
                     matchFound = true;
@@ -35,5 +37,60 @@ public class ShuttleSearch {
 
             currentTime++;
         }
+
+        List<Long> moduli = new ArrayList<>();
+        List<Long> remainders = new ArrayList<>();
+        long n = 1;
+        List<Long> nDividedByModuli = new ArrayList<>();
+        List<Long> inverses = new ArrayList<>();
+
+        for (int busId : busIds) {
+            if (busId == 0) {
+                continue;
+            }
+
+            int index = busIds.indexOf(busId);
+
+            moduli.add((long) busId);
+
+            if (index == 0) {
+                remainders.add(0L);
+            } else {
+                long remainder = busId - index;
+                remainders.add(remainder);
+            }
+        }
+
+        for (long nI : moduli) {
+            n *= nI;
+        }
+
+        for (long modulus : moduli) {
+            nDividedByModuli.add(n / modulus);
+        }
+
+        for (int i = 0; i < nDividedByModuli.size(); i++) {
+            long currentNi = nDividedByModuli.get(i);
+            long modulus = moduli.get(i);
+            long remainder = currentNi % modulus;
+            long inverse = 1;
+
+            while ((remainder * inverse) % modulus != 1) {
+                inverse++;
+            }
+
+            inverses.add(inverse);
+        }
+        
+        long x = 0;
+
+        for (int i = 0; i < remainders.size(); i++) {
+            long remainder = remainders.get(i);
+            long NI = nDividedByModuli.get(i);
+            long inverse = inverses.get(i);
+            x += remainder * NI * inverse;
+        }
+
+        System.out.println(x % n);
     }
 }
