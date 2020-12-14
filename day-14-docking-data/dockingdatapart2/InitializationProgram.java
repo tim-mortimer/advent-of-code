f7d2c66b5218dfbecb4c2eab94a81a4583c7dd1e
Tim Mortimer <tim@kiteframe.co.uk>
2020-12-14 12:08:18 +0000

    Remove unnecessary nesting

--- day-14-docking-data/dockingdatapart2/InitializationProgram.java
+++ day-14-docking-data/dockingdatapart2/InitializationProgram.java
@@ -15,61 +15,62 @@ public class InitializationProgram {
         for (String instruction: instructions) {
             if (instruction.startsWith("mask = ")) {
                 mask = instruction.substring(7);
-            } else {
-                if (mask == null) {
-                    System.out.println("Error");
-                    break;
+                continue;
+            }
+
+            if (mask == null) {
+                System.out.println("Error");
+                break;
+            }
+
+            Pattern pattern = Pattern.compile("^mem\\[(\\d+)] = (\\d+)$");
+            Matcher matcher = pattern.matcher(instruction);
+            int memoryLocation;
+            long value;
+
+            if (!matcher.find()) {
+                System.out.println("Error");
+                break;
+            }
+
+            memoryLocation = Integer.parseInt(matcher.group(1));
+            value = Integer.parseInt(matcher.group(2));
+            String bitMemoryLocation = Integer.toBinaryString(memoryLocation);
+            bitMemoryLocation = "0".repeat(36 - bitMemoryLocation.length()) + bitMemoryLocation;
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < bitMemoryLocation.length(); i++) {
+                if (mask.charAt(i) == '0') {
+                    builder.append(bitMemoryLocation.charAt(i));
+                } else {
+                    builder.append(mask.charAt(i));
+                }
+            }
+
+            String maskedMemoryLocation = builder.toString();
+
+            int xCount = 0;
+
+            for (char c: maskedMemoryLocation.toCharArray()) {
+                if (c == 'X') {
+                    xCount++;
+                }
+            }
+
+            int combinations = (int) Math.pow(2, xCount);
+
+            for (int i = 0; i < combinations; i++) {
+                String binaryString = Integer.toBinaryString(i);
+                binaryString = "0".repeat(xCount - binaryString.length()) + binaryString;
+
+                String floatingMemoryLocation = maskedMemoryLocation;
+
+                for (int j = 0; j < xCount; j++) {
+                    floatingMemoryLocation = floatingMemoryLocation.replaceFirst("X", "" + binaryString.charAt(j));
                 }
 
-                Pattern pattern = Pattern.compile("^mem\\[(\\d+)] = (\\d+)$");
-                Matcher matcher = pattern.matcher(instruction);
-                int memoryLocation;
-                long value;
-
-                if (!matcher.find()) {
-                    System.out.println("Error");
-                    break;
-                }
-
-                memoryLocation = Integer.parseInt(matcher.group(1));
-                value = Integer.parseInt(matcher.group(2));
-                String bitMemoryLocation = Integer.toBinaryString(memoryLocation);
-                bitMemoryLocation = "0".repeat(36 - bitMemoryLocation.length()) + bitMemoryLocation;
-
-                StringBuilder builder = new StringBuilder();
-
-                for (int i = 0; i < bitMemoryLocation.length(); i++) {
-                    if (mask.charAt(i) == '0') {
-                        builder.append(bitMemoryLocation.charAt(i));
-                    } else {
-                        builder.append(mask.charAt(i));
-                    }
-                }
-
-                String maskedMemoryLocation = builder.toString();
-
-                int xCount = 0;
-
-                for (char c: maskedMemoryLocation.toCharArray()) {
-                    if (c == 'X') {
-                        xCount++;
-                    }
-                }
-
-                int combinations = (int) Math.pow(2, xCount);
-
-                for (int i = 0; i < combinations; i++) {
-                    String binaryString = Integer.toBinaryString(i);
-                    binaryString = "0".repeat(xCount - binaryString.length()) + binaryString;
-
-                    String floatingMemoryLocation = maskedMemoryLocation;
-
-                    for (int j = 0; j < xCount; j++) {
-                        floatingMemoryLocation = floatingMemoryLocation.replaceFirst("X", "" + binaryString.charAt(j));
-                    }
-
-                    output.put(Long.parseLong(floatingMemoryLocation, 2), value);
-                }
+                output.put(Long.parseLong(floatingMemoryLocation, 2), value);
             }
         }
 
